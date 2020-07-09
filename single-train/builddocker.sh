b2a89b0f12513094c09c81460342f9ec177e400f
jax79sg <jax79sg@yahoo.com.sg>
2020-07-09 21:05:03 +0800

    Single working in kubernetes

--- single-train/builddocker.sh
+++ single-train/builddocker.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker build . -t image-classification-single
+docker tag image-classification-single myregistry.com:5000/image-classification-single
+docker push  myregistry.com:5000/image-classification-single
