1caaa28faf8e0349ae506ae0a29747a82ccdd635
jax79sg <jax79sg@yahoo.com.sg>
2020-07-10 12:47:10 +0800

    multi and single on kube

--- multi-train/builddocker.sh
+++ multi-train/builddocker.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker build . -t image-classification-multi
+docker tag image-classification-multi myregistry.com:5000/image-classification-multi
+docker push myregistry.com:5000/image-classification-multi 
---
+++ multi-train/runall.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+stty rows 25 && stty cols 96 && bashful run /runall.yml
